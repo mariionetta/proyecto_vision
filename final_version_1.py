0d243a621cb19e315c22e661c71dbb74c3a19ce7
mariionetta <beto.mn89@gmail.com>
2013-05-14 06:59:06 -0400

    Update final_version_1.py

--- final_version_1.py
+++ final_version_1.py
@@ -27,51 +27,6 @@ def filtro(img, pixeles, x, y):
             pixeles[i,j] = (r, g, b)
     img.save(nimg)
 
-def convolucion(img, pixeles, x, y):
-    nimg = 'cuadrado1.jpg'
-    tam = dict()
-    ancho = x
-    largo = y
-    gr = gradiente()
-    sx = gr.get(0)
-    sy = gr.get(1)
-    for i in range(x):
-        for j in range(y):
-            gx = 0
-            gy = 0
-            for k in range(len(sx[0])):
-                for l in range(len(sy[0])):
-                    try:
-                        gx +=  (pixeles[k+i, l+j][0] * sx[k][l])
-                        gy +=  (pixeles[k+i, l+j][0] * sy[k][l])
-                    except:
-                        pass
-                    Gradientex = pow(gx, 2)
-                    Gradientey = pow(gy, 2)
-                    magnitud = int(math.sqrt(Gradientex + Gradientey))
-                    angulo = math.atan2(gy, gx)
-                    rpo = ((i - ancho / 2) * math.sin(angulo)) + (j - largo / 2) * (math.cos(angulo))
-                    tam[i,j] = (angulo, rpo)
-            pixeles[i,j] = (magnitud, magnitud, magnitud)
-    img.save(nimg)
-    return tam
-
-
-def gradiente():
-    d  = dict()
-    d[0] = [ [-1,0,1], [-2,0,2], [-1,0,1] ]
-    d[1] = [ [1,2,1], [0,0,0], [-1,-2,-1] ]
-    return d
-
-def saca_angulo(img, pixeles, x, y):
-    tam  = convolucion(img, pixeles, x, y)
-    for i in range(x):
-        for j in range(y):
-            angulo = tam[i,j][0]
-
-def normaliza(img, pixeles, x, y):
-    nimg = 'cuadrado1.jpg'
-    img.save(nimg)
              
 def main():
     cam=cv.CaptureFromCAM(0)
